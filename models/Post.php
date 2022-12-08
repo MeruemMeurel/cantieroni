648ae7968acef56cb74d1f0a18464c06c81db8c8
MeruemMeurel <manuelmauro04@gmail.com>
2022-12-08 19:44:12 +0100

    Changed Models/post.php to adapt to new Post table in SQL Server

--- models/Post.php
+++ models/Post.php
@@ -8,83 +8,105 @@ class Post
 	private DateTime $ora_post;
 	private int $id_attivita;
 
-	/**
-	 * @param int $id
-	 * @param int $id_utente
-	 * @param DateTime $ora_post
-	 * @param int $id_attivita
-	 */
-	public function __construct(int $id, int $id_utente, DateTime $ora_post, int $id_attivita)
-	{
-		$this->id = $id;
-		$this->id_utente = $id_utente;
-		$this->ora_post = $ora_post;
-		$this->id_attivita = $id_attivita;
-	}
+    private string $descrizione;
 
-	/**
-	 * @return int
-	 */
-	public function getId(): int
-	{
-		return $this->id;
-	}
+    /**
+     * @param int $id
+     * @param int $id_utente
+     * @param DateTime $ora_post
+     * @param int $id_attivita
+     * @param string $descrizione
+     */
+    public function __construct(int $id, int $id_utente, DateTime $ora_post, int $id_attivita, string $descrizione)
+    {
+        $this->id = $id;
+        $this->id_utente = $id_utente;
+        $this->ora_post = $ora_post;
+        $this->id_attivita = $id_attivita;
+        $this->descrizione = $descrizione;
+    }
 
-	/**
-	 * @param int $id
-	 */
-	public function setId(int $id): void
-	{
-		$this->id = $id;
-	}
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
 
-	/**
-	 * @return int
-	 */
-	public function getIdUtente(): int
-	{
-		return $this->id_utente;
-	}
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
 
-	/**
-	 * @param int $id_utente
-	 */
-	public function setIdUtente(int $id_utente): void
-	{
-		$this->id_utente = $id_utente;
-	}
+    /**
+     * @return int
+     */
+    public function getIdUtente(): int
+    {
+        return $this->id_utente;
+    }
 
-	/**
-	 * @return DateTime
-	 */
-	public function getOraPost(): DateTime
-	{
-		return $this->ora_post;
-	}
+    /**
+     * @param int $id_utente
+     */
+    public function setIdUtente(int $id_utente): void
+    {
+        $this->id_utente = $id_utente;
+    }
 
-	/**
-	 * @param DateTime $ora_post
-	 */
-	public function setOraPost(DateTime $ora_post): void
-	{
-		$this->ora_post = $ora_post;
-	}
+    /**
+     * @return DateTime
+     */
+    public function getOraPost(): DateTime
+    {
+        return $this->ora_post;
+    }
 
-	/**
-	 * @return int
-	 */
-	public function getIdAttivita(): int
-	{
-		return $this->id_attivita;
-	}
+    /**
+     * @param DateTime $ora_post
+     */
+    public function setOraPost(DateTime $ora_post): void
+    {
+        $this->ora_post = $ora_post;
+    }
 
-	/**
-	 * @param int $id_attivita
-	 */
-	public function setIdAttivita(int $id_attivita): void
-	{
-		$this->id_attivita = $id_attivita;
-	}
+    /**
+     * @return int
+     */
+    public function getIdAttivita(): int
+    {
+        return $this->id_attivita;
+    }
+
+    /**
+     * @param int $id_attivita
+     */
+    public function setIdAttivita(int $id_attivita): void
+    {
+        $this->id_attivita = $id_attivita;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescrizione(): string
+    {
+        return $this->descrizione;
+    }
+
+    /**
+     * @param string $descrizione
+     */
+    public function setDescrizione(string $descrizione): void
+    {
+        $this->descrizione = $descrizione;
+    }
+
+
 
 
 }
