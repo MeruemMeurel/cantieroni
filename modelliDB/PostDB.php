6d334a4ab39d026a5b844c56941684997af050aa
MeruemMeurel <manuelmauro04@gmail.com>
2022-12-08 19:51:16 +0100

    Changed ModelsDB/postDB.php to adapt to new post table in SQL Server

--- modelliDB/PostDB.php
+++ modelliDB/PostDB.php
@@ -8,7 +8,8 @@ class PostDB
 	private int $id;
 	private int $id_utente;
 	private Timestamp $ora_post;
-	private int $id_attivita;
+	private int $id_cantiere;
+    private string $descrizione;
 
 	/**
 	 * Istanzia un'oggetto PostDB, passando per parametro un PDO della connessione col Database
@@ -33,85 +34,101 @@ class PostDB
 		return $stmt;
 	}
 
-	/**
-	 * @return mixed
-	 */
-	public function getConn()
-	{
-		return $this->conn;
-	}
+    /**
+     * @return mixed
+     */
+    public function getConn()
+    {
+        return $this->conn;
+    }
 
-	/**
-	 * @param mixed $conn
-	 */
-	public function setConn($conn): void
-	{
-		$this->conn = $conn;
-	}
+    /**
+     * @param mixed $conn
+     */
+    public function setConn($conn): void
+    {
+        $this->conn = $conn;
+    }
 
-	/**
-	 * @return int
-	 */
-	public function getId(): int
-	{
-		return $this->id;
-	}
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
 
-	/**
-	 * @param int $id
-	 */
-	public function setId(int $id): void
-	{
-		$this->id = $id;
-	}
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
 
-	/**
-	 * @return int
-	 */
-	public function getIdUtente(): int
-	{
-		return $this->id_utente;
-	}
+    /**
+     * @return int
+     */
+    public function getIdUtente(): int
+    {
+        return $this->id_utente;
+    }
 
-	/**
-	 * @param int $id_utente
-	 */
-	public function setIdUtente(int $id_utente): void
-	{
-		$this->id_utente = $id_utente;
-	}
+    /**
+     * @param int $id_utente
+     */
+    public function setIdUtente(int $id_utente): void
+    {
+        $this->id_utente = $id_utente;
+    }
 
-	/**
-	 * @return DateTime
-	 */
-	public function getOraPost(): Timestamp
-	{
-		return $this->ora_post;
-	}
+    /**
+     * @return Timestamp
+     */
+    public function getOraPost(): Timestamp
+    {
+        return $this->ora_post;
+    }
 
-	/**
-	 * @param DateTime $ora_post
-	 */
-	public function setOraPost(Timestamp $ora_post): void
-	{
-		$this->ora_post = $ora_post;
-	}
+    /**
+     * @param Timestamp $ora_post
+     */
+    public function setOraPost(Timestamp $ora_post): void
+    {
+        $this->ora_post = $ora_post;
+    }
 
-	/**
-	 * @return int
-	 */
-	public function getIdAttivita(): int
-	{
-		return $this->id_attivita;
-	}
+    /**
+     * @return int
+     */
+    public function getIdCantiere(): int
+    {
+        return $this->id_cantiere;
+    }
 
-	/**
-	 * @param int $id_attivita
-	 */
-	public function setIdAttivita(int $id_attivita): void
-	{
-		$this->id_attivita = $id_attivita;
-	}
+    /**
+     * @param int $id_cantiere
+     */
+    public function setIdCantiere(int $id_cantiere): void
+    {
+        $this->id_cantiere = $id_cantiere;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescrizione(): string
+    {
+        return $this->descrizione;
+    }
+
+    /**
+     * @param string $descrizione
+     */
+    public function setDescrizione(string $descrizione): void
+    {
+        $this->descrizione = $descrizione;
+    }
 
 
 }
