154bd9d5103a83d5cc596cd8b450bd420ce95f30
loregiunti <117384964+loregiunti@users.noreply.github.com>
2022-12-13 20:02:15 +0100

    implementazione classe Personale.js per la creazione della tabella delle presenze (è una prova)

---
+++ models/Personale.js
@@ -0,0 +1,14 @@
+class Personale{
+    constructor(id,nome,cognome, email, telefono, indirizzo, citta,provincia,id_ruolo,id_azienda){
+        this.id=id;
+        this.nome=nome;
+        this.cognome=cognome;
+        this.email=email;
+        this.telefono=telefono;
+        this.indirizzo=indirizzo;
+        this.citta=citta;
+        this.provincia=provincia;
+        this.id_ruolo=id_ruolo;
+        this.id_azienda=id_azienda;
+    }
+}
