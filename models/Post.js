84718f3075d98e8182d6bf4634be9d4bc6b8351d
Zhario <Vittorio.zhang.stud@tulliobuzzi.edu.it>
2022-12-14 19:11:19 +0100

    Modificato il post e inizio resoconti in html

--- models/Post.js
+++ models/Post.js
@@ -1,9 +1,10 @@
 class Post{
-    constructor(id, id_utente, ora_post, id_attivita) {
+    constructor(id, utente, ora_post, descrizione) {
+        
         this.id = id;
-        this.id_utente = id_utente;
-        this.ora_post = ora_post;
-        this.id_attivita = id_attivita;
+        this.utente = utente;
+        this.ora_post = ora_post; //TIME STAMP
+        this.descrizione = descrizione;
 
     }
 }
