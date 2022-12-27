a5e0e4aa493f0883e9e3cba144b085bc96ac8c99
iPriMeRL <lore.ucce04@gmail.com>
2022-12-27 14:49:31 +0100

    Aggiunto popup per inserire un nuovo cantiere

--- Cantieroni/js/cantInCorso.js
+++ Cantieroni/js/cantInCorso.js
@@ -10,12 +10,16 @@ nextInCorso.onclick = () => {
     document.getElementById('listaInCorso').scrollLeft += 1200;
 };
 
+function mostraInserimento(){
+    document.getElementById('inserCantiere').style.display = "block";
+}
+
 
 //Aggiunta dei cantieri nel relativo html
 var strCards = "";
 for(let i=0; i<cantInCorso.length; i++){
     if(i == 0){
-        strCards += '<div class="card bg-light" id="card1InCorso"><img class="card-img" src="../img/plus.png" alt="Card image cap"></div>'
+        strCards += '<div class="card bg-light" id="card1InCorso"><button id="plusPerAgg"><img class="card-img" src="../img/plus.png" alt="Card image cap" onclick="mostraInserimento()"></button></div>'
     }
     strCards += '<div class="card"><img class="card-img-top" src="../img/cantiere.jpg" alt="Card image cap"><div class="card-body"><p class="card-text">'+cantInCorso[i].descrizione+'</p></div></div>'
 }
