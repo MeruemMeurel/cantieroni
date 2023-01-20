2fd8182b7b5e3c7b40b11eddbac11e064d4654a2
iPriMeRL <lore.ucce04@gmail.com>
2023-01-21 00:18:44 +0100

    Il click fuori dal blocco inserimento funziona

--- Cantieroni/js/listaLavorDaAgg.js
+++ Cantieroni/js/listaLavorDaAgg.js
@@ -3,17 +3,17 @@ var plusPremutoLavor = false;
 
 //Funzione per far apparire l'inserimento (chiamata da cantInCorso.js)
 function mostraAggiungiLavCant(){
-    document.getElementById('listaLavDaAgg').style.visibility = "visible";
-    document.getElementById('listaLavDaAgg').style.top = "0";
+    document.getElementById('listaLavorDaAgg').style.visibility = "visible";
+    document.getElementById('listaLavorDaAgg').style.top = "0";
 }
 
 //Evento nel caso in cui venga premuto ESC durante l'inserimento del cantiere
 window.addEventListener('keydown', function(e) {
-    if(e.key == "Escape" && document.getElementById('listaLavDaAgg').style.visibility == "visible"){
+    if(e.key == "Escape" && document.getElementById('listaLavorDaAgg').style.visibility == "visible"){
         plusPremutoLavor = !plusPremutoLavor;
-        document.getElementById('listaLavDaAgg').style.top = "150%";
+        document.getElementById('listaLavorDaAgg').style.top = "150%";
         setTimeout(function(){
-            document.getElementById('listaLavDaAgg').style.visibility = "hidden";
+            document.getElementById('listaLavorDaAgg').style.visibility = "hidden";
         }, 300);
     }
 });
@@ -22,9 +22,9 @@ window.addEventListener('keydown', function(e) {
 window.addEventListener("click", function(e){
     if(!document.getElementById('formContentLavor').contains(e.target) && plusPremutoLavor == true){
         plusPremutoLavor = !plusPremutoLavor;
-        document.getElementById('listaLavDaAgg').style.top = "150%";
+        document.getElementById('listaLavorDaAgg').style.top = "150%";
         setTimeout(function(){
-            document.getElementById('listaLavDaAgg').style.visibility = "hidden";
+            document.getElementById('listaLavorDaAgg').style.visibility = "hidden";
         }, 300);
     }
     else if(document.getElementById('bottPerAggLav').contains(e.target)){
